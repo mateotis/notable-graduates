8be7aa0b4d2bf4a39511f7916c9dc64a3861d659
mateotis <hekfuszmate@gmail.com>
2019-11-27 14:18:25 +0400

    Split actual data set into chunks of Q-codes and modified parser to handle them accordingly.

--- wikidataParser.py
+++ wikidataParser.py
@@ -2,21 +2,37 @@ from qwikidata.linked_data_interface import get_entity_dict_from_api
 from qwikidata.entity import WikidataItem, WikidataProperty
 from time import perf_counter
 import csv
-import pandas
 
 codeList = []
 eduDict = {}
 foundCount = 0
 missingCount = 0
+readyToAppend = False
 
-f = open("top100_15_2.txt", "r")
-lines = f.read().splitlines()
-for code in lines:
-	codeList.append(code)
+# with open('MAT_alma_mater_top100_436countries2.csv', mode='r', encoding = "ISO-8859-1") as bigData: # Gets Q-codes from raw database
+#     reader = csv.reader(bigData, delimiter=',')
+#     codeCount = 0
+#     for row in reader:
+#         if codeCount == 0:
+#         	codeCount += 1
+#         else:
+#             codeList.append(row[0])
+#             codeCount += 1
 
+f = open("codeList1.txt","r")
+codes = f.read().splitlines()
+codeCount = 0
+for line in codes:
+	codeList.append(line)
+	codeCount += 1
+
+
+print('Processed', codeCount, 'Q-codes.')
 timeStart = perf_counter()
 
+cnt = 0
 for name in codeList:
+	cnt += 1
 
 	personDict = get_entity_dict_from_api(name) # Insert QCode here
 	person = WikidataItem(personDict)
@@ -26,22 +42,20 @@ for name in codeList:
 		eduGroups = claim_groups["P69"] # Grabs person's education
 		foundCount += 1
 	except:
-		#print("--------------------------")
-		print("Education not there for", person.get_label() + "!")
+		print(str(cnt) + ".", "Education not there for", person.get_label())
 		missingCount += 1
+		if(cnt % 10 == 0):
+			readyToAppend = True
 		continue
 	eduEntries = len(eduGroups) # How many different entries there are
 
-	#print("--------------------------")
-	print("Writing education for", person.get_label())
-	#print("--------------------------")
+	print(str(cnt) + ".", "Writing education for", person.get_label())
 	eduList = []
 	for i in range(eduEntries):
 		try:
 			eduEntry = eduGroups[i] # Get list entry
 			qid = eduEntry.mainsnak.datavalue.value["id"] # Get its ID
 			eduValue = WikidataItem(get_entity_dict_from_api(qid)) # Get its actual name
-			#print(eduValue.get_label()) # Print that name
 			eduList.append(eduValue.get_label())
 		except:
 			continue
@@ -49,17 +63,22 @@ for name in codeList:
 	eduDict[name] = eduList
 	eduList = []
 
+	if(cnt % 10 == 0 or readyToAppend == True): # Write to file after 100 processed entries
+		print("-------------------", '\n', "CHECKPOINT AT", cnt, '\n' + "-------------------")
+		with open('eduCSV.csv', mode='a') as eduCSV:
+			fieldnames = ['wikidata_code_B', 'education']
+			writer = csv.DictWriter(eduCSV, fieldnames=fieldnames)
+			if(cnt < 11): # Write header only at first checkpoint
+				writer.writeheader()
+			for qcode in eduDict:
+				writer.writerow({'wikidata_code_B': qcode, 'education': eduDict[qcode]})
+		eduDict = {}
+		eduList = []
+		readyToAppend = False		
+
+print("Parsed", cnt, "people.")
 print("Education found for:", foundCount)
 print("Education missing for: ", missingCount)
 
-with open('eduCSV.csv', mode='a') as eduCSV:
-	fieldnames = ['wikidata_code_B', 'education']
-	writer = csv.DictWriter(eduCSV, fieldnames=fieldnames)
-
-	writer.writeheader()
-	for qcode in eduDict:
-		writer.writerow({'wikidata_code_B': qcode, 'education': eduDict[qcode]})
-
 timeEnd = perf_counter()
 print("Execution time:", timeEnd - timeStart)
-#df = pandas.read_csv('eduCSV.csv')
